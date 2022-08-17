17185f53cdade9d402efe83d639d7adfe40a481a
Antquanique <antdancy95@gmail.com>
2022-08-16 21:24:06 -0400

    added basic 1D battleship

---
+++ battleship.js
@@ -0,0 +1,41 @@
+let randomLoc = Math.floor(Math.random() * 5);
+let location1 = randomLoc;
+let location2 = location1 + 1;
+let location3 = location2 + 1;
+
+
+let guess;
+let hits = 0;
+let guesses = 0;
+
+let isSunk = false;
+
+let stats = ""
+ 
+
+while(isSunk == false){
+    guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
+
+    if(guess < 0 || guess > 6){
+        alert("please enter a valid cell number!");
+    }else{
+        guesses += 1;
+
+        if(guess == location1 || guess == location2 || guess == location3){
+            hits += 1;
+            alert("HIT!")
+            if(hits == 3){
+                isSunk = true;
+                alert("you have sank my battleship!");
+            }
+        }else{
+            alert("MISS")
+        }
+
+       
+
+    }
+}
+
+stats = `you took ${guesses} guesses to sink the battleship which means your shooting accuracy was ${3/guesses}`;
+alert(stats)
